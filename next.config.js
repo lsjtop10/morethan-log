e7726e7a1fb9ffb8870b0514d7c954df47d02bf5
lsjtop10 <lsjtop10@gmail.com>
2025-11-17 22:03:25 +0900

    Update image domains and remove unused apple touch icon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const { OutputFileType } = require("typescript");
+
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com0', 'lh3.googleusercontent.com'],
   },
+  OutputFileType: 'standalone',
 }
